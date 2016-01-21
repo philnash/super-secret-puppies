9b5622ca9d10ee9f1e9add695b1a60466b915062
Phil Nash <philnash@gmail.com>
2016-01-21 10:29:45 +0000

    Adds puppies resource and pictures

---
+++ app/controllers/puppies_controller.rb
@@ -0,0 +1,4 @@
+class PuppiesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources 'puppies', :only => 'index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
