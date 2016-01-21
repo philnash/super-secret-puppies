1942496c147045732b613ee7912bf427bb05c7fb
Phil Nash <philnash@gmail.com>
2016-01-21 11:36:46 +0000

    Adds email config to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Email config for development
+  config.action_mailer.default_url_options = {
+    host: 'localhost',
+    post: 3000
+  }
 end
